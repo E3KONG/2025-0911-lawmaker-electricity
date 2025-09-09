9ca362063d22d9a640b4f03b1772289480f7e75d
E3KONG <ethankong.atwork@gmail.com>
2025-09-09 14:29:36 +0800

    chord: path change

--- WordCloud/vite.config.ts
+++ WordCloud/vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/twreporter/ddd/2025-0911-lawmaker-electricity/wordCloud',
+  base: '/twreporter/ddd/2025-0911-lawmaker-electricity/word-cloud',
 })
